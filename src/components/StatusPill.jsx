1f4f629ae307a41310ef4bfeb791a6f70b1fe4d0
Marcandy <marcandy.nj@gmail.com>
2026-07-06 20:38:50 -0400

    added css for different status state

--- src/components/StatusPill.jsx
+++ src/components/StatusPill.jsx
@@ -1,8 +1,18 @@
-const StatusPill = ( { status }) => {
+import styles from './StatusPill.module.css'
+
+const STATUS_CLASSES = {
+    "pending": styles.pending,
+    "complete": styles.complete,
+    "needs review": styles.needsReview,
+    "ready to bill": styles.readyToBill
+}
+
+const StatusPill = ({ status }) => {
     return (
-        <section className="statusPill">
-            <p> ==== {status} </p>
-        </section>
+                                        //if undefined give empty give '' as class 
+        <span className={`${styles.statusPill} ${STATUS_CLASSES[status] ?? ""}`}>
+           {status}
+        </span>
     )
 }
 
